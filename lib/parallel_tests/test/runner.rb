473a5481b7a1fbd0794648e5057ba865abfafdee
willemopperman <willem.n.opperman@gmail.com>
2017-01-27 13:31:41 +0200

    Use actual time

--- lib/parallel_tests/test/runner.rb
+++ lib/parallel_tests/test/runner.rb
@@ -195,7 +195,7 @@ module ParallelTests
           lines.each_with_object({}) do |line, times|
             test, time = line.split(":", 2)
             next unless test and time
-            times[test] = time.to_f if tests.include?(test)
+            times[test] = time if tests.include?(test)
           end
         end
 
